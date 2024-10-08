d23903ad1fdfeb00e7a124688942140cb7e33736
Juan Tapia Tinoco <tapiatinocojuan@gmail.com>
2024-10-07 21:14:09 -0600

    actaulizacion de codigo antes de formatear memoria microsd

--- aniadir_relays.py
+++ aniadir_relays.py
@@ -4,11 +4,11 @@ ID = "10016b7041"
 
 
 def add_subdevices(IDs):
-    data = {"deviceid": _id, "data": {}}
+    data = {"deviceid": IDs, "data": {}}
     url = "http://192.168.2.106:8081/zeroconf/add_sub_devices"
     response = requests.post(url, json=data)
     res = []
  
 
 if __name__ == "__main__":
-    add_subdivices(ID)
+    add_subdevices(ID)
